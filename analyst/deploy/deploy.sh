43a4befb74a4a65af96772490deb084092eef379
hatlonely <hatlonely@gmail.com>
2020-10-17 18:34:11 +0800

    update

--- analyst/deploy/deploy.sh
+++ analyst/deploy/deploy.sh
@@ -33,4 +33,79 @@ function CreatePullSecretsIfNotExists() {
     Warn "[kubectl create pull secret ${DockerPassword}] failed"
 }
 
+function CreateConfigMap() {
+    CreateNamespaceIfNotExists || return 1
 
+cat > tmp/${ConfigmapFile} <<EOF
+{
+  "book": {
+    "root": "data/www.shicimingju.com",
+    "out": "book.json"
+  },
+  "shiCi": {
+    "root": "data/www.shicimingju.com",
+    "out": "shici.json"
+  }
+}
+EOF
+
+    kubectl get configmap "${Configmap}" -n "${Namespace}" 2>/dev/null 1>&2 && return 0
+    kubectl create configmap "${Configmap}" -n "${Namespace}" --from-file=${ConfigmapFile}=tmp/${ConfigmapFile} &&
+    Info "[kubectl create configmap "${Configmap}" -n "${Namespace}" --from-file=${ConfigmapFile}=tmp/${ConfigmapFile}] success" ||
+    Warn "[kubectl create configmap "${Configmap}" -n "${Namespace}" --from-file=${ConfigmapFile}=tmp/${ConfigmapFile}] fail"
+}
+
+function CreateJob() {
+    cat > tmp/job.yaml <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: ${Name}
+  namespace: ${Namespace}
+spec:
+  parallelism: 1
+  completions: 1
+  activeDeadlineSeconds: 1800
+  backoffLimit: 1
+  template:
+    metadata:
+      name: ${Name}
+    spec:
+      imagePullSecrets:
+      - name: ${PullSecrets}
+      containers:
+      - name: ${Name}
+        imagePullPolicy: Always
+        image: ${Image}:${Version}
+        command: [ "bin/analyst", "-c", "config/shicimingju.json" ]
+        volumeMounts:
+        - name: ${Name}-data
+          mountPath: /var/docker/crawler/data
+        - name: ${Name}-config
+          mountPath: /var/docker/crawler/config
+      volumes:
+      - name: ${Name}-data
+        persistentVolumeClaim:
+          claimName: ${PVCName}
+      - name: ${Name}-config
+        projected:
+          sources:
+          - configMap:
+              name: ${Configmap}
+              items:
+                - key: ${ConfigmapFile}
+                  path: shicimingju.json
+      restartPolicy: OnFailure
+EOF
+    kubectl apply -f tmp/job.yaml &&
+    Info "[kubectl apply -f tmp/job.yaml] success" ||
+    Warn "[kubectl apply -f tmp/job.yaml] failed"
+}
+
+function main() {
+    CreateConfigMap || return 2
+    CreatePullSecretsIfNotExists || return 3
+    CreateJob
+}
+
+main "$@"
